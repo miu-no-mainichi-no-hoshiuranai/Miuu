c169066d25c9309784bc58cb6bc705e9fb709201
miu-no-mainichi-no-hoshiuranai <164460659+miu-no-mainichi-no-hoshiuranai@users.noreply.github.com>
2024-03-23 19:12:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 window.onload = function() {
     var images = [
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/1.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/2.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/3.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/4.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/5.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/6.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/7.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/8.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/9.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/10.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/12.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/13.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/14.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/15.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/16.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/17.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/18.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/19.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/20.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/21.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/22.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/23.webp',
-        'https://raw.githubusercontent.com/miu-no-mainichi-no-hoshiuranai/Miuu/main/src/24.webp',
+'https://i.postimg.cc/fyDJnQVw/1.webp';
+'https://i.postimg.cc/nrFcSKWr/10.webp';
+'https://i.postimg.cc/T1t2zL89/11.webp';
+'https://i.postimg.cc/RCd0683'h/12.webp';
+'https://i.postimg.cc/TYP3r9m'h/13.webp';
+'https://i.postimg.cc/ZKDKjdxY/14.webp';
+'https://i.postimg.cc/L8Y4W8xt/15.webp';
+'https://i.postimg.cc/wjCxYYrS/16.webp';
+'https://i.postimg.cc/3w28Kb5W/17.webp';
+'https://i.postimg.cc/76DxbMw9/18.webp';
+'https://i.postimg.cc/MTDzBMGS/19.webp';
+'https://i.postimg.cc/'ht6v4dXZ/2.webp';
+'https://i.postimg.cc/prLRJW9n/20.webp';
+'https://i.postimg.cc/brV867bV/21.webp';
+'https://i.postimg.cc/RCgMV'h81/22.webp';
+'https://i.postimg.cc/ncMnsLby/23.webp';
+'https://i.postimg.cc/F'hLmZnKG/24.webp';
+'https://i.postimg.cc/cJ3'hvLYx/3.webp';
+'https://i.postimg.cc/Yq2'hdkcR/4.webp';
+'https://i.postimg.cc/c'hMLckXP/5.webp';
+'https://i.postimg.cc/dQj'hR3fD/6.webp';
+'https://i.postimg.cc/bN6dMMp'h/7.webp';
+'https://i.postimg.cc/KzyYB'hrn/8.webp';
+'https://i.postimg.cc/Nj5Lw4jr/9.webp';
     ];
     var randomImage = images[Math.floor(Math.random() * images.length)];
     document.getElementById('random-image').src = randomImage;
